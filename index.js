fd3fe2e94ed9fba46fd4aa0d7d2a1a1ddf1e7021
codedsleek <103906821+codedsleek@users.noreply.github.com>
2024-01-05 13:43:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var navLinks = document.getElementById("nav");
 var mobileBtnExit = document.querySelector("#mobile-exit");
 
 mobileBtn.addEventListener("click", function () {
-    nav.classList.add("menu-btn");
+    navLinks.classList.add("menu-btn");
 })
 
 mobileBtnExit.addEventListener("click", function () {
-    nav.classList.remove("menu-btn");
+    navLinks.classList.remove("menu-btn");
 })
 
 
