f950f4e735805f5bc9e523b1cb187627caa9b170
codedsleek <103906821+codedsleek@users.noreply.github.com>
2024-01-05 15:00:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
-    navLinks.style.right = "0";
+    navLinks.style.display = "none";
     }
 function hideMenu() {
     navLinks.style.display ="none";
