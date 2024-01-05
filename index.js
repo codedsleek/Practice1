800e8825cd133f1a64f846d042070174f4f4badc
codedsleek <103906821+codedsleek@users.noreply.github.com>
2024-01-05 13:45:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var mobileBtn = document.querySelector("#navLinks");
-var navLinks = document.getElementById("nav");
+var nav = document.getElementById("nav");
 var mobileBtnExit = document.querySelector("#mobile-exit");
 
 mobileBtn.addEventListener("click", function () {
-    navLinks.classList.add("menu-btn");
+    nav.classList.add("menu-btn");
 })
 
 mobileBtnExit.addEventListener("click", function () {
-    navLinks.classList.remove("menu-btn");
+    nav.classList.remove("menu-btn");
 })
 
 
