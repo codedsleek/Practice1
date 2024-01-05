bff4cbafbd74754dad4afdbe13cf447514252112
codedsleek <103906821+codedsleek@users.noreply.github.com>
2024-01-05 14:31:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var navLinks = document.getElementById("navLinks");
+var isMenuVisible = false;
+
+function toggleMenu() {
+   if (isMenuVisible) {
+       hideMenu();
+   } else {
+       showMenu();
+   }
+}
+
 function showMenu() {
     navLinks.style.right = "0";
-    }
-function hideMenu() {
-    navLinks.style.right ="-300px";
-    }
+    isMenuVisible = true;
+}
 
+function hideMenu() {
+    navLinks.style.right = "-200px";
+    isMenuVisible = false;
+}
