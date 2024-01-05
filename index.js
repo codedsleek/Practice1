7e4c046e674561f9238901714c7cb493a81ae1c6
codedsleek <103906821+codedsleek@users.noreply.github.com>
2024-01-05 14:39:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 var navLinks = document.getElementById("navLinks");
-var isMenuVisible = false;
-
-function toggleMenu() {
-   if (isMenuVisible) {
-       hideMenu();
-   } else {
-       showMenu();
-   }
-}
-
 function showMenu() {
     navLinks.style.right = "0";
-    isMenuVisible = true;
-}
-
+    }
 function hideMenu() {
-    navLinks.style.right = "-200px";
-    isMenuVisible = false;
-}
+    navLinks.style.right ="-200px";
+    }
